dd91282052d8d47566c10fa0aec0a7149926714c
Rodney Bejabeng <Rodneyb2@illinois.edu>
2022-08-16 23:03:10 -0500

    added database

--- test.py
+++ test.py
@@ -2,8 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name": "video", "views": 100000})
-print(response.json())
-input()
-response = requests.get(BASE + "video/1")
-print(response.json())
+response = requests.patch(BASE + "video/2", {})
+print(response)
